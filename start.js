86dcca6f5fff114c69e242c2bfc894451d47bda1
Maximka <maxim.kraev@gmail.com>
2013-09-07 15:11:56 +0400

    first commit

---
+++ start.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var http = require('http');
+var config = require('./config');
+var stream = require('./lib/stream');
+var playlist = require('./lib/playlist');
+var iniparser = require('iniparser');
+
+var cfgPath = process.argv[3] || './config.ini';
+
+iniparser.parse(cfgPath, function (err, data) {
+	config.set(data);
+	start();
+});
+
+function start() {
+	playlist.start();
+	http.createServer(function (req, res) {
+		var pathname = req.url;
+
+		if (pathname === '/playlist') {
+			req.setEncoding('utf8');
+			playlist.request(res);
+			return;
+		}
+
+		var parts = pathname.split('/');
+		console.log(parts);
+		if (parts.length > 1 && parts[1] === 'stream') {
+			stream.attach(req, res, parts[2]);
+			return;
+		}
+
+		// not found handle
+		res.writeHead(404, {
+			'Content-Type': 'text/plain'
+		});
+		res.end('NotFound\n');
+
+	}).listen(config.port, '0.0.0.0');
+
+	console.log('Server running at ' + config.local_domain);
+}
