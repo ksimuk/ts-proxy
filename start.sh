868b31b7addfc99a5ef6c31cb5a207576fd887f4
Maxim Kraev <maxim.kraev@gmail.com>
2013-12-19 17:26:26 +0400

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run -u stream -v `pwd`:/home/stream:rw -rm=true -p 0.0.0.0:18080:18080 -p 0.0.0.0:28080:28080 -i -t ts-proxy /home/stream/start-internal.sh
+sudo docker run -u stream -v `pwd`:/home/stream:rw -rm=true -p 18080 -p 28080 -i -t ts-proxy /home/stream/start-internal.sh
